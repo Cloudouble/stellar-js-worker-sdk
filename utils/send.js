861c944215cd9d09f225e69aa7e7d1ef7d277111
David Pitman <david.pitman@cloudouble.com>
2024-05-07 13:02:44 +1000

    building

--- utils/send.js
+++ utils/send.js
@@ -1,50 +1,102 @@
-export default {
-    base32Chars: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567',
-    algorithms: { PK: 0, Hash: 0 },
-    keyTypeMap: {
-        STRKEY_PUBKEY: [6 << 3, 'PK'], STRKEY_MUXED: [12 << 3, 'PK'], STRKEY_PRIVKEY: [18 << 3, 'PK'],
-        STRKEY_PRE_AUTH_TX: [19 << 3, 'Hash'], STRKEY_HASH_X: [23 << 3, 'Hash'],
-        STRKEY_SIGNED_PAYLOAD: [15 << 3, 'PK'], STRKEY_CONTRACT: [2 << 3, 'Hash']
+const base32Chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567'
+
+const keyTypeMap = {
+    STRKEY_PUBKEY: [6 << 3, 'PK'], STRKEY_MUXED: [12 << 3, 'PK'], STRKEY_PRIVKEY: [18 << 3, 'PK'],
+    STRKEY_PRE_AUTH_TX: [19 << 3, 'Hash'], STRKEY_HASH_X: [23 << 3, 'Hash'],
+    STRKEY_SIGNED_PAYLOAD: [15 << 3, 'PK'], STRKEY_CONTRACT: [2 << 3, 'Hash']
+}
+
+const base32Decode = base32String => {
+    let [bytes, bits, value, index, i] = [[], 0, 0, 0, 0]
+    for (; i < base32String.length; i++) {
+        [value, bits] = [(value << 5) | (index = base32Chars.indexOf(base32String[i])), bits + 5]
+        if (index === -1) throw new Error('Invalid base-32 character')
+        while (bits >= 8) {
+            bytes.push(value >>> (bits - 8) & 0xFF)
+            bits -= 8
+            value &= (1 << bits) - 1
+        }
+    }
+    return new Uint8Array(bytes)
+}
+
+const base32Encode = bytes => {
+    let [base32String, bits, value, index, i] = ['', 0, 0, 0, 0]
+    for (; i < bytes.length; i++) {
+        [value, bits] = [(value << 8) | bytes[i], bits + 8]
+        while (bits >= 5) [base32String, bits] = [base32String + base32Chars[index = (value >>> (bits - 5)) & 31], bits - 5]
+    }
+    if (bits > 0) [index, base32String] = [(value << (5 - bits)) & 31, base32String + base32Chars[index]]
+    return base32String
+}
+
+const addressToPublicKeyBytes = addressString => {
+    let [bytes, addressBytes, memoBytes, payloadBytes] = [base32Decode(addressString)], keyType
+    for (const k in keyTypeMap) if (base32Encode([keyTypeMap[k][0]])[0] === addressString[0]) { keyType = k; break }
+    bytes = bytes.slice(1, -2)
+    addressBytes = bytes.slice(0, 32)
+    if (keyType === 'STRKEY_MUXED') memoBytes = bytes.slice(0, 8)
+    if (keyType === 'STRKEY_SIGNED_PAYLOAD') payloadBytes = bytes.slice(4, 4 + (new DataView(bytes.buffer, 0, 4)).getUint32(0, false))
+    return [addressBytes, memoBytes, payloadBytes, keyType]
+}
+
+const operationFieldProcessors = {
+    sourceAccount: account => {
+        return { ed25519: addressToPublicKeyBytes(account)[0], type: 'KEY_TYPE_ED25519' }
+    }
+}
+
+const operationFieldProcessorMap = {
+    CREATE_ACCOUNT: { destination: operationFieldProcessors.sourceAccount },
+    PAYMENT: { destination: 'string', asset: 'string', amount: 'number' },
+    PATH_PAYMENT_STRICT_RECEIVE: { sendAsset: 'string', sendMax: 'number', destination: 'string', destAsset: 'string', destAmount: 'number', path: 'json' },
+    MANAGE_SELL_OFFER: { selling: 'string', buying: 'string', amount: 'number', price: 'string', offerID: 'number' },
+    CREATE_PASSIVE_SELL_OFFER: { selling: 'string', buying: 'string', amount: 'number', price: 'string' },
+    SET_OPTIONS: {
+        inflationDest: 'string', clearFlags: 'number', setFlags: 'number', masterWeight: 'number', lowThreshold: 'number',
+        medThreshold: 'number', highThreshold: 'number', homeDomain: 'string', signer: 'json'
     },
+    CHANGE_TRUST: { line: 'string', limit: 'number' },
+    ALLOW_TRUST: { trustor: 'string', asset: 'string', authorize: 'number' },
+    ACCOUNT_MERGE: { destination: 'string' },
+    INFLATION: {},
+    MANAGE_DATA: { dataName: 'string', dataValue: 'json' },
+    BUMP_SEQUENCE: { bumpTo: 'number' },
+    MANAGE_BUY_OFFER: { selling: 'string', buying: 'string', buyAmount: 'number', price: 'string', offerId: 'number' },
+    PATH_PAYMENT_STRICT_SEND: { sendAsset: 'string', sendAmount: 'number', destination: 'string', destAsset: 'string', destMin: 'number', path: 'json' },
+    CREATE_CLAIMABLE_BALANCE: { asset: 'string', amount: 'number', claimants: 'json' },
+    CLAIM_CLAIMABLE_BALANCE: { balanceID: 'string' },
+    BEGIN_SPONSORING_FUTURE_RESERVES: { sponsoredID: 'string' },
+    END_SPONSORING_FUTURE_RESERVES: {},
+    REVOKE_SPONSORSHIP: { ledgerKey: 'json', signer: 'json' },
+    CLAWBACK: { asset: 'string', from: 'string', amount: 'number' },
+    CLAWBACK_CLAIMABLE_BALANCE: { balanceID: 'string' },
+    SET_TRUST_LINE_FLAGS: { trustor: 'string', asset: 'string', clearFlags: 'number', setFlags: 'number' },
+    LIQUIDITY_POOL_DEPOSIT: { liquidityPoolID: 'string', maxAmountA: 'number', maxAmountB: 'number', minPrice: 'string', maxPrice: 'string' },
+    LIQUIDITY_POOL_WITHDRAW: { liquidityPoolID: 'string', maxAmountA: 'number', maxAmountB: 'number', minPrice: 'string', maxPrice: 'number' },
+    INVOKE_HOST_FUNCTION: { contractAddress: 'string', functionName: 'string', args: 'json' },
+    EXTEND_FOOTPRINT_TTL: { extendTo: 'number' },
+    RESTORE_FOOTPRINT: {}
+}
+
+
+
+export default {
+    base32Chars,
+    algorithms: { PK: 0, Hash: 0 },
+    keyTypeMap,
+    operationFieldProcessors,
     crc16: function (bytes) {
         let [crc, i, j] = [0x0000, 0, 0]
         for (; i < bytes.length; i++) for ((crc ^= bytes[i] << 8, j = 0); j < 8; j++) crc = ((crc & 0x8000) !== 0)
             ? (((crc << 1) & 0xFFFF) ^ 0x1021) : ((crc << 1) & 0xFFFF)
         return new Uint8Array([crc & 0xFF, crc >> 8])
     },
-    base32Decode: function (base32String) {
-        let [bytes, bits, value, index, i] = [[], 0, 0, 0, 0]
-        for (; i < base32String.length; i++) {
-            [value, bits] = [(value << 5) | (index = this.base32Chars.indexOf(base32String[i])), bits + 5]
-            if (index === -1) throw new Error('Invalid base-32 character')
-            while (bits >= 8) {
-                bytes.push(value >>> (bits - 8) & 0xFF)
-                bits -= 8
-                value &= (1 << bits) - 1
-            }
-        }
-        return new Uint8Array(bytes)
-    },
-    base32Encode: function (bytes) {
-        let [base32String, bits, value, index, i] = ['', 0, 0, 0, 0]
-        for (; i < bytes.length; i++) {
-            [value, bits] = [(value << 8) | bytes[i], bits + 8]
-            while (bits >= 5) [base32String, bits] = [base32String + this.base32Chars[index = (value >>> (bits - 5)) & 31], bits - 5]
-        }
-        if (bits > 0) [index, base32String] = [(value << (5 - bits)) & 31, base32String + this.base32Chars[index]]
-        return base32String
-    },
-    addressToPublicKeyBytes: function (addressString) {
-        let [bytes, addressBytes, memoBytes, payloadBytes] = [this.base32Decode(addressString)], keyType
-        for (const k in this.keyTypeMap) if (this.base32Encode([this.keyTypeMap[k][0]])[0] === addressString[0]) { keyType = k; break }
-        bytes = bytes.slice(1, -2)
-        addressBytes = bytes.slice(0, 32)
-        if (keyType === 'STRKEY_MUXED') memoBytes = bytes.slice(0, 8)
-        if (keyType === 'STRKEY_SIGNED_PAYLOAD') payloadBytes = bytes.slice(4, 4 + (new DataView(bytes.buffer, 0, 4)).getUint32(0, false))
-        return [addressBytes, memoBytes, payloadBytes, keyType]
-    },
+    base32Decode,
+    base32Encode,
+    addressToPublicKeyBytes,
     bytesPublicKeyToAddress: function (addressBytes, memoBytes = [], payloadBytes = [], keyType = 'STRKEY_PUBKEY') {
-        const bytes = [this.keyTypeMap[keyType][0] | this.algorithms[this.keyTypeMap[keyType][1]], ...addressBytes]
+        const bytes = [keyTypeMap[keyType][0] | this.algorithms[keyTypeMap[keyType][1]], ...addressBytes]
         if ((keyType === 'STRKEY_MUXED') && memoBytes && memoBytes.length) bytes.push(...memoBytes)
         if ((keyType === 'STRKEY_SIGNED_PAYLOAD') && payloadBytes && payloadBytes.length) {
             const payloadLengthView = new DataView(new ArrayBuffer(4))
@@ -52,12 +104,11 @@ export default {
             bytes.push(...(new Uint8Array(payloadLengthView.buffer)), ...payloadBytes, ...(new Uint8Array(4 - payloadBytes.length % 4)).fill(0))
         }
         bytes.push(...this.crc16(bytes))
-        return this.base32Encode(new Uint8Array(bytes))
+        return base32Encode(new Uint8Array(bytes))
     },
     createTransactionSourceObject: async function (transactionSimpleObject) {
-        console.log('line 58', transactionSimpleObject)
         const transactionSourceObject = {
-            sourceAccount: { ed25519: this.addressToPublicKeyBytes(transactionSimpleObject.sourceAccount)[0], type: 'KEY_TYPE_ED25519' },
+            sourceAccount: { ed25519: addressToPublicKeyBytes(transactionSimpleObject.sourceAccount)[0], type: 'KEY_TYPE_ED25519' },
             ext: { v: 0 },
             fee: transactionSimpleObject.fee,
             memo: { type: 'MEMO_NONE' },
@@ -117,15 +168,21 @@ export default {
             }
         }
         for (const operation of (transactionSimpleObject.operations ?? [])) {
-            const { type, op } = operation
+            const { type, op } = operation, { sourceAccount } = op
+            if (sourceAccount) delete op.sourceAccount
+            if (type in operationFieldProcessorMap) {
+                for (const p in op) {
+                    if (p in operationFieldProcessorMap[type]) {
+                        op[p] = operationFieldProcessorMap[type][p](op[p])
+                    }
+                }
+            }
             const operationProperty = type.toLowerCase().split('_').map((v, i) => i ? `${v[0].toUpperCase()}${v.slice(1)}` : v).join('') + 'Op'
-            const operationObject = {
-                body: { type, [operationProperty]: { ...op } }
+            const operationObject = { body: { type, [operationProperty]: { ...op } } }
+            if (sourceAccount) {
+                operationObject.sourceAccount = operationFieldProcessors.sourceAccount(sourceAccount)
             }
-            if (op.sourceAccount) {
-                delete operationObject.body[[operationProperty]].sourceAccount
-                operationObject.sourceAccount = { ed25519: this.addressToPublicKeyBytes(op.sourceAccount)[0], type: 'KEY_TYPE_ED25519' }
-            }
+
             transactionSourceObject.operations.push(operationObject)
         }
         if (transactionSimpleObject.sorobanData) transactionSourceObject.ext = { v: 1, sorobanData: transactionSimpleObject.sorobanData }
